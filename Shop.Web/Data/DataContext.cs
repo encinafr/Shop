25deeeac91f58a49efff056d1d2256cab18aa4e6
Javier Encina <encinajavier7@gmail.com>
2019-04-16 22:12:31 -0300

    Add identityUser

--- Shop.Web/Data/DataContext.cs
+++ Shop.Web/Data/DataContext.cs
@@ -1,13 +1,14 @@
 ﻿
 namespace Shop.Web.Data
 {
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
     using Shop.Web.Data.Entities;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DbSet<Product> Products { get; set; }
 
